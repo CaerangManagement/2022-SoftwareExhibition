3a55ca4bd485c7a28c67e01933fcf051d58fc35d
j2an777 <110087099+j2an777@users.noreply.github.com>
2022-08-31 16:32:03 +0900

    0831_1

--- public/js/main.js
+++ public/js/main.js
@@ -39,6 +39,6 @@ $(document).ready(function($) {
   $(".scroll_move").click(function(event){
       console.log(".scroll_move");         
       event.preventDefault();
-      $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+      $('html,body').animate({scrollTop:$(this.hash).offset().top}, 100);
   });
 });
