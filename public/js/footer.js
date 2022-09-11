91fbb7b1bb65b53c6397ad341e35c9573328ed92
j2an <ha991104@gmail.com>
2022-09-11 19:17:49 +0900

    main_update

---
+++ public/js/footer.js
@@ -0,0 +1,37 @@
+$(document).ready(function($) {
+    $('#role').click(() => {
+        let pw = prompt('관리자 비밀번호를 입력해주세요')
+    
+        $.ajax({
+          method: 'post',
+          url: '/auth/role',
+          data: {
+            pw: pw
+          }
+        }).done(function (결과) {
+          alert('관리자 계정으로 전환 되었습니다.')
+          location.href = '/'
+        }).fail(function (a, b, c) {
+          alert('비밀번호가 일치하지 않습니다.')
+        });
+    
+      })
+    
+      $('#back').click(() => {
+        let pw = prompt('관리자 비밀번호를 입력해주세요')
+    
+        $.ajax({
+          method: 'post',
+          url: '/auth/back',
+          data: {
+            pw: pw
+          }
+        }).done(function (결과) {
+          alert('일반 계정으로 전환 되었습니다.')
+          location.href = '/'
+        }).fail(function (a, b, c) {
+          alert('비밀번호가 일치하지 않습니다.')
+        });
+    
+      });
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,39 +1,4 @@
-$(document).ready(function($) {
-  $('#role').click(() => {
-    let pw = prompt('관리자 비밀번호를 입력해주세요')
-
-    $.ajax({
-      method: 'post',
-      url: '/auth/role',
-      data: {
-        pw: pw
-      }
-    }).done(function (결과) {
-      alert('관리자 계정으로 전환 되었습니다.')
-      location.href = '/'
-    }).fail(function (a, b, c) {
-      alert('비밀번호가 일치하지 않습니다.')
-    });
-
-  })
-
-  $('#back').click(() => {
-    let pw = prompt('관리자 비밀번호를 입력해주세요')
-
-    $.ajax({
-      method: 'post',
-      url: '/auth/back',
-      data: {
-        pw: pw
-      }
-    }).done(function (결과) {
-      alert('일반 계정으로 전환 되었습니다.')
-      location.href = '/'
-    }).fail(function (a, b, c) {
-      alert('비밀번호가 일치하지 않습니다.')
-    });
-
-  })
+$(document).ready(function() {
 
   //헤더 메뉴 누르면 그 영역에 스크롤 내려가게 하기
   $(".scroll_move").click(function(event){
@@ -43,77 +8,93 @@ $(document).ready(function($) {
   });
 
   //section2 스크롤 인지 후 애니메이션
-  $('.section2_box1').each(function(){
-        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-        
-        if( bottom_of_window > bottom_of_object/2 ){
-            $(this).animate({'opacity':'1'},2000);
-        }
+  $(window).scroll( function() {
+    $('.section2_box1').each(function(i){
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      
+      if( bottom_of_window > bottom_of_object/2 ){
+          $(this).animate({'opacity':'1'},1000);
+      }
+    });
   }); 
   
-  $('.section2_box2').each(function(){
-        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-        
-        if( bottom_of_window > bottom_of_object/2 ){
-            $(this).animate({'opacity':'1'},3000);
-        }
+  $(window).scroll( function() {
+    $('.section2_box2').each(function(i){
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      
+      if( bottom_of_window > bottom_of_object/2 ){
+          $(this).animate({'opacity':'1'},1000);
+      }
+    });
   }); 
    
-  $('.section2_box3').each(function(){
-        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-        
-        if( bottom_of_window > bottom_of_object/2 ){
-            $(this).animate({'opacity':'1'},4000);
-        }
+  $(window).scroll( function() {
+    $('.section2_box3').each(function(i){
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      
+      if( bottom_of_window > bottom_of_object/2 ){
+          $(this).animate({'opacity':'1'},1000);
+      }
+    });
   }); 
 
   //section3 스크롤 인지 후 애니메이션
-  $('.team1_select').each(function() {
-        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
+  $(window).scroll( function() {
+    $('.team1_select').each(function(i){
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      
+      if( bottom_of_window > bottom_of_object ){
+          $(this).animate({'opacity':'1'},1000);
+      }
+    });
+  }); 
 
-        if( bottom_of_window > bottom_of_object/2) {
-            $(this).animate({'opacity':'1'},2000);
-        }
-  });
+  $(window).scroll( function() {
+    $('.team2_select').each(function(i){
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      
+      if( bottom_of_window > bottom_of_object ){
+          $(this).animate({'opacity':'1'},1000);
+      }
+    });
+  }); 
 
-  $('.team2_select').each(function() {
-        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
+  $(window).scroll( function() {
+    $('.team3_select').each(function(i){
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      
+      if( bottom_of_window > bottom_of_object ){
+          $(this).animate({'opacity':'1'},1000);
+      }
+    });
+  }); 
 
-        if( bottom_of_window > bottom_of_object/2) {
-            $(this).animate({'opacity':'1'},2000);
-        }
-  });
+  $(window).scroll( function() {
+    $('.team4_select').each(function(i){
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      
+      if( bottom_of_window > bottom_of_object ){
+          $(this).animate({'opacity':'1'},1000);
+      }
+    });
+  }); 
 
-  $('.team3_select').each(function() {
-        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-        if( bottom_of_window > bottom_of_object/2) {
-            $(this).animate({'opacity':'1'},2000);
-        }
-  });
-
-  $('.team4_select').each(function() {
-        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-        if( bottom_of_window > bottom_of_object/2) {
-            $(this).animate({'opacity':'1'},2000);
-        }
-  });
-
-  $('.team5_select').each(function() {
-        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-        if( bottom_of_window > bottom_of_object/2) {
-            $(this).animate({'opacity':'1'},2000);
-        }
-  });
+  $(window).scroll( function() {
+    $('.team5_select').each(function(i){
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      
+      if( bottom_of_window > bottom_of_object ){
+          $(this).animate({'opacity':'1'},1000);
+      }
+    });
+  }); 
 
 });
